f94469b9ea06c2c657feba2f94884304003af574
Roger Santos <roger_santos@outlook.com>
2015-09-21 13:50:51 -0300

    Commit teste

---
+++ Executar EXE - BAT.sql
@@ -0,0 +1,22 @@
+EXEC sp_configure 'show advanced options', 1;
+GO
+
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'xp_cmdshell', 1;
+GO
+
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'show advanced options', 0;
+GO
+
+RECONFIGURE;
+GO
+
+--TESTE
+EXECUTE xp_cmdshell 'C:\Workspace\delete.bat';
+
+ok google
