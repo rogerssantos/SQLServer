efa4da2566d76bcbefa054ca1ea0d8b2a2549309
Roger Santos <roger_santos@outlook.com>
2015-11-02 18:24:32 -0200

    alter

--- Commands/Select all colluns by table for all tables.sql
+++ Commands/Select all colluns by table for all tables.sql
@@ -1,8 +1,8 @@
 SELECT 'select ' + 
-stuff((select ', ' + c.column_name from INFORMATION_SCHEMA.COLUMNS C 
-where C.TABLE_NAME = T.TABLE_NAME
-for xml path('')), 1, 1, '') [INFORMATION_SCHEMA.TABLES/INFORMATION_SCHEMA.COLUMNS], 
- ' from ' + LOWER(T.TABLE_NAME)  + ';' as 'table_name'
+stuff((SELECT ', ' + c.column_name FROM INFORMATION_SCHEMA.COLUMNS C 
+		WHERE C.TABLE_NAME = T.TABLE_NAME
+		FOR XML PATH('')), 1, 1, '') [INFORMATION_SCHEMA.TABLES/INFORMATION_SCHEMA.COLUMNS],
+		' from ' + LOWER(T.TABLE_NAME)  + ';' AS 'table_name'
 FROM INFORMATION_SCHEMA.TABLES T 
-where t.table_name like '%'
-order by table_name asc
+WHERE t.table_name like '%'
+ORDER BY table_name ASC;
