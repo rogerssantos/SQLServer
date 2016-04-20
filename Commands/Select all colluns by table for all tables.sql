0ce9d1f9b44246eaf3e7adfeb6226782b6b26373
Roger Santos <roger_santos@outlook.com>
2016-04-20 13:31:55 -0300

    Update Select all colluns by table for all tables.sql

--- Commands/Select all colluns by table for all tables.sql
+++ Commands/Select all colluns by table for all tables.sql
@@ -1,8 +1,8 @@
 SELECT 'SELECT' + 
 STUFF((SELECT ', ' + c.column_name FROM INFORMATION_SCHEMA.COLUMNS C 
 		WHERE C.TABLE_NAME = T.TABLE_NAME
-		FOR XML PATH('')), 1, 1, '') [INFORMATION_SCHEMA.TABLES/INFORMATION_SCHEMA.COLUMNS],
-		'FROM ' + LOWER(T.TABLE_NAME)  + ';' AS 'table_name'
+		FOR XML PATH('')), 1, 1, '')
+		+ ' FROM ' + T.TABLE_SCHEMA + '.' + LOWER(T.TABLE_NAME) + ';' AS 'table_name'
 FROM INFORMATION_SCHEMA.TABLES T 
-WHERE t.table_name like '%'
+--WHERE t.table_name like '<table_name>'
 ORDER BY table_name ASC;
