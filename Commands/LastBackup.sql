63eca6699febe9704aa2816dbf68106b80886ba5
Roger Santos <roger_santos@outlook.com>
2016-05-04 11:27:46 -0300

    .

--- Commands/LastBackup.sql
+++ Commands/LastBackup.sql
@@ -1,7 +1,7 @@
 -- Verifying the date of last backup made
 
 SELECT sdb.Name AS DatabaseName,
-COALESCE(CONVERT(VARCHAR(12), MAX(bus.backup_finish_date), 101),’-‘) AS LastBackUpTime
+	COALESCE(CONVERT(VARCHAR(12), MAX(bus.backup_finish_date), 101),’-‘) AS LastBackUpTime
 FROM sys.sysdatabases sdb
 LEFT OUTER JOIN msdb.dbo.backupset bus ON bus.database_name = sdb.name
 GROUP BY sdb.Name
