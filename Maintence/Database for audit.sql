b9a9a7f2af58312fd696068ec98a3b4db9e637a0
Roger Santos <roger_santos@outlook.com>
2016-05-30 20:44:48 -0300

    COMMIT

--- Maintence/Database for audit.sql
+++ Maintence/Database for audit.sql
@@ -26,3 +26,54 @@ CREATE TABLE [dbo].[DDLEvents](
 	[LoginName] [nvarchar](255) NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+
+IF OBJECT_ID('TR_DDLEvents') IS NOT NULL
+	DROP TRIGGER TR_DDLEvents;
+GO
+
+CREATE TRIGGER TR_DDLEvents
+    ON DATABASE
+    FOR CREATE_PROCEDURE, ALTER_PROCEDURE, DROP_PROCEDURE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE
+        @EventData XML = EVENTDATA();
+ 
+    DECLARE 
+        @ip VARCHAR(32) =
+        (
+            SELECT client_net_address
+                FROM sys.dm_exec_connections
+                WHERE session_id = @@SPID
+        );
+ 
+    INSERT AuditDB.dbo.DDLEvents
+    (
+		EventDate,
+        EventType,
+        EventDDL,
+        EventXML,
+        DatabaseName,
+        SchemaName,
+        ObjectName,
+        HostName,
+        IPAddress,
+        ProgramName,
+        LoginName
+    )
+    SELECT
+		CURRENT_TIMESTAMP,
+        @EventData.value('(/EVENT_INSTANCE/EventType)[1]',   'NVARCHAR(100)'), 
+        @EventData.value('(/EVENT_INSTANCE/TSQLCommand)[1]', 'NVARCHAR(MAX)'),
+        @EventData,
+        DB_NAME(),
+        @EventData.value('(/EVENT_INSTANCE/SchemaName)[1]',  'NVARCHAR(255)'), 
+        @EventData.value('(/EVENT_INSTANCE/ObjectName)[1]',  'NVARCHAR(255)'),
+        HOST_NAME(),
+        @ip,
+        PROGRAM_NAME(),
+        SUSER_SNAME();
+END
+GO
